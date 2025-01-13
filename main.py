d692043af4ccfade1f44c10a46ce4289ddbb69a5
Travis <137155991+travisvc@users.noreply.github.com>
2025-01-13 10:27:57 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Predictor pool optimization for real-time Random Forest
+# Group 4 - Case Study in Econometrics and Data Science
+# Sarah Dick - 2637856
+# Anne-Britt Analbers - 2662375
+# Amrohie Ramsaran - 2763388 
+# Travis van Cornewal - 2731231
+
+print('Hello world')
